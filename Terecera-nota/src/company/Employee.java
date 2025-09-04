1d1a6fd5f4121038e00ee9be82f21cfcea69256d
estefany <suarezcondeestefanygabriela@gmail.com>
2025-09-03 19:48:53 -0500

    nuevo ejercicio

---
+++ Terecera-nota/src/company/Employee.java
@@ -0,0 +1,12 @@
+package company;
+
+public class Employee {
+    public String name;
+    public double salary;
+    public String role;
+}
+
+public Employee(String name, double salary, String role){
+    this.name = name;
+    
+}
